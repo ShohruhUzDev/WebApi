e77ebbbc7d1a8a4cdd1f8a2f5c7ca8ae0d6b06aa
ShohruhUzDev <shohruh7464784@mail.ru>
2021-07-10 12:01:00 +0500

    add for car service layer

---
+++ WebApi.Backend/ServiceLayer/CarService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Backend.Models;
+
+namespace WebApi.Backend.ServiceLayer
+{
+    public class CarService : ICarService
+    {
+        private DataConext _dataContext;
+
+        public CarService(DataConext dataConext)
+        {
+            _dataContext = dataConext;
+        }
+        public Car Get(int id)
+        {
+            return _dataContext.Cars.FirstOrDefault(i => i.Id == id);
+
+        }
+
+        public IEnumerable<Car> GetAll()
+        {
+            var cars = _dataContext.Cars.ToList();
+            return cars;
+        }
+    }
+}
---
+++ WebApi.Backend/ServiceLayer/ICarService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Backend.Models;
+
+namespace WebApi.Backend.ServiceLayer
+{
+    interface ICarService
+    {
+        Car Get(int id);
+        IEnumerable<Car> GetAll();
+
+    }
+}
