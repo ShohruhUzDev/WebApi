ea14e08fddb3ece2c0054ca65ccf025867934436
ShohruhUzDev <shohruh7464784@mail.ru>
2021-07-10 11:54:40 +0500

    add for Person service layer

---
+++ WebApi.Backend/ServiceLayer/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Backend.Models;
+
+namespace WebApi.Backend.ServiceLayer
+{
+    interface IPersonService
+    {
+        Person Get(int id);
+        IEnumerable<Person> GetAll();
+        Person GetPersonWithCar(int id);
+    }
+}
---
+++ WebApi.Backend/ServiceLayer/PersonService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Backend.Models;
+
+namespace WebApi.Backend.ServiceLayer
+{
+    public class PersonService : IPersonService
+    {
+        private readonly DataConext _datacontext;
+        public PersonService(DataConext dataConext)
+        {
+            this._datacontext = dataConext;
+        }
+        public Person Get(int id)
+        {
+           var person= _datacontext.Persons.FirstOrDefault(i => i.Id == id);
+
+            return person;
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            var persons = _datacontext.Persons.ToList();
+            return persons;
+        }
+
+        public Person GetPersonWithCar(int id)
+        {
+            var person = _datacontext.Persons.Include(i => i.Cars).FirstOrDefault(j => j.Id == id);
+            return person;
+        }
+    }
+}
