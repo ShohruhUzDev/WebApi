0c8ee9c9d401c5de147ec191e5872740538d4948
ShohruhUzDev <shohruh7464784@mail.ru>
2021-07-10 11:06:23 +0500

    add Car and Person classes

---
+++ WebApi.Backend/Models/Car.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Backend.Models
+{
+    [Table("car")]
+    public class Car
+    {
+        [Key]
+        [Required]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        [MaxLength(20)]
+        public string  Name { get; set; }
+
+
+        [Column("color")]
+        [Required]
+        [MaxLength(20)]
+        public string  Color { get; set; }
+        public int? PersonId { get; set; }
+        public Person Person { get; set; }
+
+
+
+    }
+}
---
+++ WebApi.Backend/Models/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Backend.Models
+{
+    [Table("person")]
+    public class Person
+    {
+        [Key]
+        [Required]
+        [Column("id")]
+        public int Id { get; set; }
+        
+        [Column("firstname")]
+        [Required]
+       [MaxLength(20)]
+        public string  FirstName { get; set; }
+
+        [Column("lasttname")]
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Column("phone")]
+        [Required]
+       
+        public string Phone { get; set; }
+
+        public List<Car> Cars { get; set; }
+
+    }
+}
